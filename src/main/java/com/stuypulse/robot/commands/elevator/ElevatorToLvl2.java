3c6bf2b37102c88d75928ff4162498d7c550095d
Zixi Qiao <qiaozixi@gmail.com>
2025-02-01 15:40:53 -0500

    deleted extra command

--- src/main/java/com/stuypulse/robot/commands/elevator/ElevatorToLvl2.java
+++
@@ -1,9 +0,0 @@
-package com.stuypulse.robot.commands.elevator;
-
-import com.stuypulse.robot.constants.Settings.Elevator;
-
-public class ElevatorToLvl2 extends ElevatorToHeight{
-    public ElevatorToLvl2Alt(){
-        super(Elevator.L2_HEIGHT_METERS);
-    }
-}
