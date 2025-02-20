d7387f9dcd8e53299568fd88131e7479a47ced66
Ian Shi <Ianshi1026@gmail.com>
2025-02-20 14:15:31 -0500

    Camera offsets and names

--- src/main/java/com/stuypulse/robot/constants/Cameras.java
+++ src/main/java/com/stuypulse/robot/constants/Cameras.java
@@ -8,8 +8,8 @@ import edu.wpi.first.math.util.Units;
 public interface Cameras {
 
     public CameraInfo[] LimelightCameras = new CameraInfo[] {
-        new CameraInfo("funnel", new Pose3d(Units.inchesToMeters(-13.819), Units.inchesToMeters(2.250), Units.inchesToMeters(6.829), new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(-20), Units.degreesToRadians(0)))),
-        new CameraInfo("shooter", new Pose3d(Units.inchesToMeters(9.0), Units.inchesToMeters(9.226), Units.inchesToMeters(10.0), new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(-13.1), Units.degreesToRadians(-18.65))))
+        new CameraInfo("limelight-funnel", new Pose3d(Units.inchesToMeters(-13.819), Units.inchesToMeters(2.250), Units.inchesToMeters(6.829), new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(-20), Units.degreesToRadians(0)))),
+        new CameraInfo("limelight-shooter", new Pose3d(Units.inchesToMeters(9.0), Units.inchesToMeters(9.226), Units.inchesToMeters(10.0), new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(-13.1), Units.degreesToRadians(-18.65))))
     };
 
     public static class CameraInfo {
