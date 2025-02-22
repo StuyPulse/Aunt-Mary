ded93aca3a512fb62336c93614d6ecbdc9cf2443
Ian Shi <Ianshi1026@gmail.com>
2025-02-22 13:04:03 -0500

    Put climb in brake mode

--- src/main/java/com/stuypulse/robot/constants/Motors.java
+++ src/main/java/com/stuypulse/robot/constants/Motors.java
@@ -98,7 +98,7 @@ public interface Motors {
         TalonFXConfig MOTOR_CONFIG = new TalonFXConfig()
 			.withCurrentLimitAmps(80)
 			.withRampRate(0.25)
-			.withNeutralMode(NeutralModeValue.Coast)
+			.withNeutralMode(NeutralModeValue.Brake)
 			.withInvertedValue(InvertedValue.Clockwise_Positive)
 			.withPIDConstants(Gains.Climb.PID.kP, Gains.Climb.PID.kI, Gains.Climb.PID.kD, 0)
 			.withFFConstants(Gains.Climb.FF.kS, Gains.Climb.FF.kV, Gains.Climb.FF.kA, Gains.Climb.FF.kG, 0)
