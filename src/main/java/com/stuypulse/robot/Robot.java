55ce7524b7c171aa5847995c99883321e472ff0b
Ian Shi <Ianshi1026@gmail.com>
2025-02-27 20:38:32 -0500

    Oops forgot to schedule the command

--- src/main/java/com/stuypulse/robot/Robot.java
+++ src/main/java/com/stuypulse/robot/Robot.java
@@ -39,7 +39,7 @@ public class Robot extends TimedRobot {
         robot = new RobotContainer();
         DataLogManager.start();
         // Ignore barge tags
-        new VisionSetWhiteList(1, 2, 3, 6, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 21, 22);
+        new VisionSetWhiteList(1, 2, 3, 6, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 21, 22).schedule();
     }
 
     @Override
