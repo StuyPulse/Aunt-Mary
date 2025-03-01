f206e7ce20392e30abad1b0ae047e04f25dc813a
jopy-man <jophyw2008@gmail.com>
2025-03-01 10:24:01 -0500

    elastic

--- src/main/java/com/stuypulse/robot/Robot.java
+++ src/main/java/com/stuypulse/robot/Robot.java
@@ -20,6 +20,7 @@ import edu.wpi.first.util.PixelFormat;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -52,6 +53,9 @@ public class Robot extends TimedRobot {
     @Override
     public void robotPeriodic() {
         CommandScheduler.getInstance().run();
+
+        SmartDashboard.putNumber("Match Time", DriverStation.getMatchTime());
+
     }
 
     /*********************/
