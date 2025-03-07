ec785d6c5da693da2ed0f85e69e3a62ce732abc0
SP-COMPuter <se@stuypulse.com>
2025-03-07 18:02:17 -0500

    Zoomy zoomy auton

--- src/main/java/com/stuypulse/robot/commands/autons/IKLA/FourPieceIKLA.java
+++ src/main/java/com/stuypulse/robot/commands/autons/IKLA/FourPieceIKLA.java
@@ -123,7 +123,7 @@ public class FourPieceIKLA extends SequentialCommandGroup {
                 new SwerveDrivePIDToBranchScore(CoralBranch.A, 4, true)
                     .withTranslationalConstraints(5.85, Settings.Swerve.Alignment.Constraints.MAX_ACCELERATION_AUTON.get() + 0.75)
                     .withTimeout(4)
-                    .deadlineFor(new LEDApplyPattern(CoralBranch.I.isLeftPeg() ? Settings.LED.LEFT_SIDE_COLOR : Settings.LED.RIGHT_SIDE_COLOR)),
+                    .deadlineFor(new LEDApplyPattern(CoralBranch.A.isLeftPeg() ? Settings.LED.LEFT_SIDE_COLOR : Settings.LED.RIGHT_SIDE_COLOR)),
                 new WaitUntilCommand(() -> Shooter.getInstance().hasCoral())
                     .andThen(
                         new ElevatorToL4Front().alongWith(new ArmToL4Front())
