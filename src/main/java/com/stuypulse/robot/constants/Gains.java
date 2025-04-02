91c6816d2e7b25ed9ad2ee3a566d448b79789cb8
SP-COMPuter <se@stuypulse.com>
2025-04-02 17:06:26 -0400

    Separate coral and algae gains for the arm, increase kP for coral gains a little

--- src/main/java/com/stuypulse/robot/constants/Gains.java
+++ src/main/java/com/stuypulse/robot/constants/Gains.java
@@ -34,7 +34,22 @@ public interface Gains {
     }
 
     public interface Arm {
-        public interface CoralAlgae  {
+        public interface Coral  {
+            public interface PID {
+                double kP = 0.15744 * 360;
+                double kI = 0.0 * 360;
+                double kD = 0.015 * 360;
+            }
+
+            public interface FF {
+                double kS = 0.2272; 
+                double kV = 0.0095901 * 360;
+                double kA = 0.0015361 * 360;
+                double kG = 0.69269;
+            }
+        }
+
+        public interface Algae  {
             public interface PID {
                 double kP = 0.095744 * 360;
                 double kI = 0.0 * 360;
