48f426f2d3e72fdc7df830c612b994616cb44ffc
SP-COMPuter <se@stuypulse.com>
2025-06-11 15:24:45 -0400

    removed stuff

--- src/main/java/com/stuypulse/robot/commands/autons/FDCB/FDCENudge.java
+++ src/main/java/com/stuypulse/robot/commands/autons/FDCB/FDCENudge.java
@@ -7,7 +7,6 @@ import com.stuypulse.robot.commands.shooter.ShooterStop;
 import com.stuypulse.robot.commands.superStructure.SuperStructureFeed;
 import com.stuypulse.robot.commands.superStructure.SuperStructureWaitUntilAtTarget;
 import com.stuypulse.robot.commands.superStructure.coral.SuperStructureCoralL4Front;
-import com.stuypulse.robot.commands.swerve.pidToPose.coral.SwerveDriveCoralScoreAlignAuton;
 import com.stuypulse.robot.commands.swerve.pidToPose.coral.SwerveDriveCoralScoreAlignWithClearance;
 import com.stuypulse.robot.commands.swerve.pidToPose.coral.SwerveDrivePIDToBranchScore;
 import com.stuypulse.robot.constants.Settings;
