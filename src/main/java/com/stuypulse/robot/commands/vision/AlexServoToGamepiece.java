396ac98e0541e628598165036a8c3e61d025f745
Kalimul Kaif <kalimulkaif@gmail.com>
2025-12-04 23:42:17 -0500

    Faster rotation at further distances and slower translation when approaching closer + add constants for scaling; TODO for meeting at top

--- src/main/java/com/stuypulse/robot/commands/vision/AlexServoToGamepiece.java
+++ src/main/java/com/stuypulse/robot/commands/vision/AlexServoToGamepiece.java
@@ -17,7 +17,6 @@ import com.stuypulse.robot.subsystems.climb.Climb.ClimbState;
 import com.stuypulse.robot.subsystems.swerve.CommandSwerveDrivetrain;
 import com.stuypulse.robot.subsystems.vision.LimelightVision;
 import com.stuypulse.robot.subsystems.vision.ServoObjectData;
-import com.stuypulse.stuylib.control.angle.AngleController;
 import com.stuypulse.stuylib.control.angle.feedback.AnglePIDController;
 import com.stuypulse.stuylib.input.Gamepad;
 import com.stuypulse.stuylib.math.Angle;
@@ -31,6 +30,7 @@ import com.stuypulse.stuylib.streams.vectors.filters.VDeadZone;
 import com.stuypulse.stuylib.streams.vectors.filters.VLowPassFilter;
 import com.stuypulse.stuylib.streams.vectors.filters.VRateLimit;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
@@ -38,6 +38,15 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class AlexServoToGamepiece extends Command {
+    /* TODO (12/5/25)
+     * - Test PID gain switching for rotation
+     *     Expected Behavior: rotates aggressively from further distances and
+     *     normalizes as it gets closer to the gamepiece
+     * - Test scaling of forward velocity
+     *     Expected Behavior: travels at normal speed from further distances
+     *     but slows down as it gets closer to gamepiece
+     * Tune the constants at the top of the file to test!
+     */
 
     private final CommandSwerveDrivetrain swerve;
     private final LimelightVision vision;
@@ -47,20 +56,23 @@ public class AlexServoToGamepiece extends Command {
     private double angleOfGamepiece;
     private Rotation2d robotHeading;
 
-    private final AngleController angleController;
+    private final AnglePIDController angleController;
     private final VStream linearVelocity;
     private final IStream angularVelocity;
 
     private final static double kP_VEL_PARALLEL = 0.5;
     private final static double kP_VEL_FORWARD = 0.25;
+    private final static double PID_SCALING = 20.0; // < 20% of frame = faster rotation
+    private final static double FORWARD_VEL_SCALING = 20.0; // > 20% of frame = slow down translation
 
     public AlexServoToGamepiece(Gamepad driver) {
         swerve = CommandSwerveDrivetrain.getInstance();
         vision = LimelightVision.getInstance();
         this.driver = driver;
         offset = new Rotation2d(Cameras.LimelightCameras[2].getLocation().getRotation().getZ());
-        angleController = new AnglePIDController(Alignment.THETA.kP, Alignment.THETA.kI, Alignment.THETA.kD)
-                .setSetpointFilter(new AMotionProfile(Settings.Swerve.Alignment.Constraints.DEFAULT_MAX_VELOCITY,
+
+        angleController = new AnglePIDController(Alignment.THETA.kP, Alignment.THETA.kI, Alignment.THETA.kD);
+        angleController.setSetpointFilter(new AMotionProfile(Settings.Swerve.Alignment.Constraints.DEFAULT_MAX_VELOCITY,
                         Settings.Swerve.Alignment.Constraints.DEFAULT_MAX_ANGULAR_ACCELERATION));
 
         linearVelocity = VStream.create(this::getDriverInputAsVelocity)
@@ -86,7 +98,6 @@ public class AlexServoToGamepiece extends Command {
 
     @Override
     public void initialize() {
-        
     }
 
     @Override
@@ -98,6 +109,14 @@ public class AlexServoToGamepiece extends Command {
             Rotation2d froggyHeading = robotHeading.minus(Rotation2d.fromDegrees(90.0));
             Rotation2d txnc = Rotation2d.fromDegrees(frame.txncOfHighestArea());
             double ta = frame.getHighestArea();
+            double scaleFactor = 1.0;
+            if (ta > 5.0) {
+                scaleFactor = MathUtil.clamp(Math.max(1.0, PID_SCALING / ta), 1.0, 1.5);
+            }
+            
+            angleController.setP(Alignment.THETA.kP * scaleFactor);
+            angleController.setI(Alignment.THETA.kI);
+            angleController.setD(Alignment.THETA.kD * Math.sqrt(scaleFactor));
 
             double unWrappedAngle = robotHeading.getDegrees();
             if (unWrappedAngle < 0.0) {
@@ -117,7 +136,8 @@ public class AlexServoToGamepiece extends Command {
                 Math.sin(Units.degreesToRadians(offset.getDegrees()+ 90.0)))
                     .mul(speed_parallel).rotate(Angle.fromDegrees(swerveTargetAngle));
 
-            double speed_forward = kP_VEL_FORWARD * 1.0/ta;
+            // Consider scaling this forward velocity in a non-linear manner
+            double speed_forward = kP_VEL_FORWARD * MathUtil.clamp(FORWARD_VEL_SCALING/ta, 0.40, 1.0);
             Vector2D vel_forward = new Vector2D(
                 froggyHeading.getCos(),
                 froggyHeading.getSin())
